084d2867396a83352c4132b0457f61ab338bd458
Stevy <664553716@qq.com>
2018-03-10 02:57:58 +0800

    add mod func

--- apps/mdadm/func.py
+++ apps/mdadm/func.py
@@ -1,7 +1,25 @@
 # coding = utf-8
+import sys
+import inspect
 from iadslib.IadsBaseContext import IadsBaseContext
 
 
 class MdadmManager(IadsBaseContext):
-	pass
+	def __init__(self,
+	             volume_name="iads_raid",
+	             raid_level=0,
+	             disks=None,
+	             conf_file="/etc/mdadm.conf"):
+		super(MdadmManager, self).__init__()
+		self.raid_level = raid_level
+		if isinstance(disks, dict):
+			if len(dict) < 2:
+				self.err("The number of disks is less than 2.")
+		else:
+			disks = None
+			self.err("%s param disks is not a dict." % self.__class__.__name__)
+		self.disks = disks
+		self.volume_name = volume_name
+		self.conf_name = conf_file
+		
 
